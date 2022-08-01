3b4610782f4c9a189be34e705f7492a0c42fc10a
black <antariuz@gmail.com>
2022-08-01 11:24:42 +0300

    Initial commit

---
+++ src/main/java/avada/media/usainua_admin/UsainuaAdminApplication.java
@@ -0,0 +1,13 @@
+package avada.media.usainua_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsainuaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsainuaAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/avada/media/usainua_admin/UsainuaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package avada.media.usainua_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsainuaAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
