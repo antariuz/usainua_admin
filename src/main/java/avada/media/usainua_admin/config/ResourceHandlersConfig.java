f929bf9d4e26c4fa6b0aa0a1ee9feba257b6bd4a
black <antariuz@gmail.com>
2022-08-04 16:17:29 +0300

    Handlers refactor

--- src/main/java/avada/media/usainua_admin/config/ResourceHandlersConfig.java
+++ src/main/java/avada/media/usainua_admin/config/ResourceHandlersConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
@@ -12,18 +11,12 @@ public class ResourceHandlersConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String uploadPath = System.getProperty("os.name").contains("Linux") ?
+                Paths.get("/var/lib/tomcat9/uploaded/").toFile().getAbsolutePath() : Paths.get("uploaded").toFile().getAbsolutePath();
+        //        Upload folder
+        registry.addResourceHandler("/uploaded/**").addResourceLocations("file://" + uploadPath + "/");
+        //        Static folder
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        exposeDirectory("uploaded", registry);
-    }
-
-    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-        Path uploadDir = Paths.get("uploaded");
-        if (System.getProperty("os.name").contains("Linux")) {
-            uploadDir = Paths.get("/var/lib/tomcat9/uploaded/");
-        }
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
-        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file://" + uploadPath + "/");
     }
 
 }
